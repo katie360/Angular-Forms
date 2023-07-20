8aae17386d38a039c1cb75f94633d8e5860b97a0
katie360 <kateratemo44@gmail.com>
2023-07-20 16:05:21 +0300

    server changes

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const PORT = 4000;
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Define the POST route for "/enroll" before any catch-all route
+app.post('/enroll', function(req, res) {
+  console.log(req.body);
+  res.status(200).send({ "message": "data received" });
+});
+
+// Define other routes or middleware below
+app.get('/', function(req, res) {
+  res.send('Hello from server');
+});
+
+// Add more routes or middleware as needed...
+
+// Catch-all route (if needed)
+// This route will handle any request that doesn't match the routes defined above
+app.use(function(req, res) {
+  res.status(404).send("Not Found");
+});
+
+app.listen(PORT, function() {
+  console.log("Server running on localhost:" + PORT);
+});
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 import { User } from '../user';
-
+import { EnrollmentService } from '../enrollment.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
+  constructor(private _enrollmentService: EnrollmentService){
+  }
 
   topics = ['Angular','React','Vue'];
 
-  userModel = new User('kate','kate@gmail',12345,'','Morning',true)
+  userModel = new User('kate','kate@gmail',12345,'default','Morning',true)
+
+  topicHasError = true;
+  validateTopic(value:any){
+    if (value === 'default'){
+      this.topicHasError = true
+    }
+    else{
+      this.topicHasError = false
+    }
+
+  }
+
+  onSubmit(){
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+        data => console.log ("success!" ,data),
+        error => console.log ("Error!",error)
+    )
+
+  }
 
 }
